6784557ae264eb3a2dbc5fa39ca6c6d25d808a92
DESKTOP-MEQB8TK <oluchysaviour@gmail.com>
2024-09-29 15:03:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("javascript is on")
+const button = document.querySelector("button")
+console.log(button)
+const numbercontainer =document.querySelector (".number-container")
+console.log(numbercontainer)
+let counter =0
+function addbutton(){
+
+   if (number < 10) 
+   counter++;
+console.log ("button is clicked"+ counter + "times")
+numbercontainer,textcontent ="number is clicked" + counter + "times"
+button.addEventListener("click",signbutton)
+}
+button.addEventListener("click",addbutton)
+const number =10
+if (number < 5) {
+    console.log("yes low count number")
+} else if (number === 5) {
+    console.log("medium count number");
+
+} else {
+    console.log("maximum count number")
+}
+
+
